430550023e68821eba6d060651e785db09f1a9ec
Kyle Oliveri <KyleOliveri@Kyles-Mac-Pro.local>
2019-05-23 01:01:38 -0700

    Updated resume, projects, and removed section

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    $("#contactBtn2").on("click", function () {
+
+        $(".nameInput").val("");
+        $("#email").val("");
+        $(".messageInput").val("");
+
+    });
+
+});
